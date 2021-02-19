1a13c5a5b8d81942c46a2313ef7f21e4ecdc85df
uesmgn <ug.0114@icloud.com>
2021-02-20 08:19:11 +0900

    Update logging.py

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -19,22 +19,21 @@ def setup(ax, **kwargs):
 
 class LossLogger:
     def __init__(self):
-        self.stats = {}
+        self.stats = defaultdict(list)
 
     def update(self, **kwargs):
         for key, value in kwargs.items():
-            if key not in self.stats:
-                self.stats[key] = defaultdict(list)
             self.stats[key].append(value)
 
     def items(self):
         return self.stats.items()
 
-    def save(self, values, epoch, out, **kwargs):
+    def save(self, key, epoch, out, **kwargs):
         if epoch > 1:
             fig, ax = plt.subplots()
-            xx = np.linspace(0, epoch, len(values))
-            ax.plot(xx, values)
+            yy = self.stats[key]
+            xx = np.linspace(0, epoch, len(yy))
+            ax.plot(xx, yy)
             setup(ax, **kwargs)
             plt.tight_layout()
             plt.savefig(out)
